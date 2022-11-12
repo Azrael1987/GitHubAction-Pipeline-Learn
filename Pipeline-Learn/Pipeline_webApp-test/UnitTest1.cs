3d37eeb5e33f1bc89ac6b0c2b0341f8473497994
Adam <gruszecki.adam@gmail.com>
2022-11-12 17:31:51 +0100

    init commit - Web App + test

---
+++ Pipeline-Learn/Pipeline_webApp-test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace PipelineWebApp.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void isTrue()
+        {
+            Assert.True(true);
+        }
+    }
+}
